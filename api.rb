a5c75442518ac32caa77647e4d1467f555b234de
Tony Collen <tonyc@users.noreply.github.com>
2022-11-22 19:58:56 -0600

    Update api.rb

--- api.rb
+++ api.rb
@@ -1,5 +1,5 @@
 # The weather API url is:
-#  https://api.open-meteo.com/v1/forecast?latitude=44.52&longitude=-93.67&hourly=temperature_2m,weathercode,surface_pressure,windspeed_10m,winddirection_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&past_days=5
+# https://api.open-meteo.com/v1/forecast?latitude=44.52&longitude=-93.67&hourly=temperature_2m,weathercode,surface_pressure,windspeed_10m,winddirection_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&past_days=5
 
 
 # Write your code here!
